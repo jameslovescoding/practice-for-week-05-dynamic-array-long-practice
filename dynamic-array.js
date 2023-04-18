c2772d419e75d994933018227e68d1d87449c45d
Casey <spoocter@gmail.com>
2023-04-18 17:25:00 -0400

    Completed

--- dynamic-array.js
+++ dynamic-array.js
@@ -12,17 +12,18 @@ class DynamicArray {
   }
 
   push(val) {
+    if (this.length === this.capacity) {
+      this.resize();
+    }
     this.data[this.length] = val;
     this.length++;
-    // if (this.length === this.capacity) {
-    //   this.resize();
-    // }
     return this.length;
     // Your code here
   }
 
 
   pop() {
+    if (this.length === 0) return undefined;
     this.length--;
     let ele = this.data[this.length];
     this.data[this.length] = undefined;
@@ -31,23 +32,45 @@ class DynamicArray {
   }
 
   shift() {
-
+    if (this.length === 0) return undefined;
+    let ele = this.data[0];
+    for (let i = 1; i < this.length; i++){
+      this.data[i-1] = this.data[i];
+    }
+    this.length--;
+    return ele;
     // Your code here
   }
 
   unshift(val) {
-
+    if (this.length === this.capacity) this.resize();
+    for (let i = this.length; i > 0; i--){
+      this.data[i] = this.data[i-1];
+      }
+      this.data[0] = val;
+      this.length++;
+      return this.length;
     // Your code here
   }
 
   indexOf(val) {
-
+    for (let i = 0; i < this.length; i++){
+      if (this.data[i] === val) return i;
+    }
+    return -1;
     // Your code here
   }
 
   resize() {
-
-    // Your code here
+    // Your code here 0b00000100
+    //15 => 16, 16; 16 => 17, 17; 17 => 18, 18; 18 => 19, 19 //32! - 16!
+    //15 =>16, 32; 16 => 17, 1
+    this.capacity *= 2;
+    let newArray = new Array(this.capacity);
+    for (let i = 0; i < this.length; i++){
+      newArray[i] = this.data[i];
+    }
+    this.data = newArray;
   }
 
 }
