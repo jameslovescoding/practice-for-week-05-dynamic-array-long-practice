f65b14d500f9db549199d51e36f67fb70a96662a
James Yu Cao <caoyu19920414@gmail.com>
2023-04-18 16:52:54 -0400

    finished first half

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,23 +1,32 @@
 class DynamicArray {
 
   constructor(defaultSize = 4) {
-
-    // Your code here
+    this.length = 0;
+    this.capacity = defaultSize;
+    this.data = new Array(defaultSize);
   }
 
   read(index) {
-
+    return this.data[index];
     // Your code here
   }
 
   push(val) {
-
+    this.data[this.length] = val;
+    this.length++;
+    // if (this.length === this.capacity) {
+    //   this.resize();
+    // }
+    return this.length;
     // Your code here
   }
 
 
   pop() {
-
+    this.length--;
+    let ele = this.data[this.length];
+    this.data[this.length] = undefined;
+    return ele;
     // Your code here
   }
 
